ed4fe6c8f1098908ce6668af5269b2d06918f0a4
Ankit Amonkar <ankit.amonkar@gmail.com>
2019-11-08 12:34:54 -0500

    initial

--- src/Die.java
+++ src/Die.java
@@ -5,4 +5,13 @@ public class Die {
         die.roll();
         System.out.println(die.getNumDots());
     }
+    public Die(){
+
+    }
+    public int roll(){
+        int x = (int) Math.random()*6+1;
+        int y = (int) Math.random()*6+1;
+        return x;
+    }
+
 }
